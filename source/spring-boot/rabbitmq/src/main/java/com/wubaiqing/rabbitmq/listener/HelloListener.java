0e6190c159cbe413270ab307792bce13785503cb
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-06 18:29:14 +0800

    fix: 修复 Rabbit 无法消费问题

--- source/spring-boot/rabbitmq/src/main/java/com/wubaiqing/rabbitmq/listener/HelloListener.java
+++ source/spring-boot/rabbitmq/src/main/java/com/wubaiqing/rabbitmq/listener/HelloListener.java
@@ -1,9 +1,11 @@
 package com.wubaiqing.rabbitmq.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RabbitListener(queues = "wubaiqing")
 public class HelloListener {
 
     @RabbitHandler
