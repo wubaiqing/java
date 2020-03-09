f016239a3da0a4ec7bd61ea74f87c92780f57b5d
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-09 22:00:31 +0800

    feat: 新增 banner 案例

---
+++ source/spring-boot/banner/src/test/java/com/wubaiqing/banner/BannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wubaiqing.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
