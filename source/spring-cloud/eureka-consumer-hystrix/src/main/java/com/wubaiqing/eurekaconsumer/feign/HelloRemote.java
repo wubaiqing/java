66c93227282c1b5f4d3dd9731f2c00ee192e3ec5
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-17 15:18:23 +0800

    feat: 修改消费者和支持熔断的案例分开

--- source/spring-cloud/eureka-consumer-hystrix/src/main/java/com/wubaiqing/eurekaconsumer/feign/HelloRemote.java
+++ source/spring-cloud/eureka-consumer-hystrix/src/main/java/com/wubaiqing/eurekaconsumer/feign/HelloRemote.java
@@ -1,11 +1,12 @@
 package com.wubaiqing.eurekaconsumer.feign;
 
+import com.wubaiqing.eurekaconsumer.hystrix.HelloRemoteHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "spring-cloud-provider")
+@FeignClient(name = "spring-cloud-provider", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
