7448d5219624ad7698359fc5d517873a0b3323b6
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-17 15:12:18 +0800

    feat: 新增熔断案例

--- source/spring-cloud/eureka-consumer/src/main/java/com/wubaiqing/eurekaconsumer/IndexController.java
+++ source/spring-cloud/eureka-consumer/src/main/java/com/wubaiqing/eurekaconsumer/IndexController.java
@@ -3,6 +3,7 @@ package com.wubaiqing.eurekaconsumer;
 import com.wubaiqing.eurekaconsumer.feign.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,9 @@ public class IndexController {
     @Autowired
     HelloRemote helloRemote;
 
-    @GetMapping("hello")
-    public String index() {
-        return helloRemote.hello("wubaiqing");
+    @GetMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
     }
 
 }
